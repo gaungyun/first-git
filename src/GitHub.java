89e8a65fd2fa712bbf26bd1c4cd3d763a12bbda4
gaungyun <17610827712@163.com>
2017-12-31 22:01:10 +0800

    GitHub主要完成的是常用变量的封装

---
+++ src/GitHub.java
@@ -0,0 +1,5 @@
+public class GitHub {
+    private static final String NAME="gitHub.com";
+    private static  final String emil = "gangyun@163.com";
+    private static  final String password = "*******";
+}
