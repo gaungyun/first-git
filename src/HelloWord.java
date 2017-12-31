dc96dca07a982c1f2c78efa7ac717d19d506fc81
gaungyun <17610827712@163.com>
2017-12-31 21:52:35 +0800

    自己测试的第一个git

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,7 +2,11 @@ public class HelloWord {
     static {
         System.out.println("你好gitHub");
     }
+
     public static void main(String[] args){
         System.out.println("Hello Word Git Hub");
+        for(int i = 0; i< 10; i++){
+            System.out.println(i + "AA");
+        }
     }
 }
